77a12fd276126ddce76dfc76e4c29413ab1552eb
liuhe <liuheb@meixing.com>
2024-10-08 20:57:10 +0800

    add SpawnerExample Demo

---
+++ Assets/SpawnerExample/Script/Spawner.cs
@@ -0,0 +1,13 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace SpawnerExample.Script
+{
+    public struct Spawner : IComponentData
+    {
+        public Entity Prefab;
+        public float3 SpawnPosition;
+        public float NextSpawnTime;
+        public float SpawnRate;
+    }
+}
